f16ab3b05972e63d85ec58e9a0586e6f5719d0c8
David Okunlola <nybryx@yahoo.co.uk>
2017-01-04 01:42:59 +0000

    Remodal blur bg | animate colours | shuffle

--- app/js/main.js
+++ app/js/main.js
@@ -1,238 +1,227 @@
 'use strict';
-var ColorMixer = require( './ColorMixer.js' );
+var ColorMixer = require('./ColorMixer.js');
 var cm = new ColorMixer();
-var _ = require( 'lodash' );
-// var _ = require( './lodash.custom.min.js' );
-
-var log = (function (){
-	return function(msg){
+// var _ = require( 'lodash' );
+var _ = require('./lodash.custom.min.js');
+var log = (function() {
+	return function(msg) {
 		console.log(msg);
 	}
 }());
-
-var app = angular.module( 'app', [ 'ngAnimate' ] );
-
+var app = angular.module('app', ['ngAnimate']);
 mainCtrl.$inject = ['$scope', '$timeout'];
-
-app.controller( 'mixer', mainCtrl );
-
-app.directive( 'setColour', function() {
+app.controller('mixer', mainCtrl);
+app.directive('setColour', function() {
 	return {
 		scope: {
 			clr: '='
 		},
-		link: function( scope, tElem, tAttrs ) {
-			var $elem = angular.element( tElem );
-			var rgb = tAttrs.rgb ? toRgbString( JSON.parse( tAttrs.rgb ).rgb ) : 'rgb(0,0,0)';
-			$elem.find( 'div' ).css( 'background-color', rgb );
+		link: function(scope, tElem, tAttrs) {
+			var $elem = angular.element(tElem);
+			var rgb = tAttrs.rgb ? toRgbString(JSON.parse(tAttrs.rgb).rgb) : 'rgb(0,0,0)';
+			$elem.find('div').css('background-color', rgb);
 		}
 	}
 });
-
-app.directive( 'updateColour', function() {
+app.directive('updateColour', function() {
 	return {
-		link: function( scope, tElem, tAttrs ) {
-			var $elem = angular.element( tElem );
+		link: function(scope, tElem, tAttrs) {
+			var $elem = angular.element(tElem);
 			var cellBroadcastLookup = {
 				'mix': 'update-mix',
 				'goal': 'update-goal'
 			};
-			scope.$on( cellBroadcastLookup[ tAttrs.cellName ], function( evt, args ) {
-				$elem.css( 'background-color', args );
-			} );
+			scope.$on(cellBroadcastLookup[tAttrs.cellName], function(evt, args) {
+				$elem.css('background-color', args);
+			});
 		}
 	}
 });
-
-app.animation('.repeated', function ($timeout){
-  console.log('anim')
-
-  return {
-    enter: function (element, done){
-		var $elem = $(element);
-	    $elem.css('z-index',100);
-		$elem.snabbt({
-			easing:'easeIn',
-			opacity: 1,
-			fromOpacity: 0,
-			fromPosition: [0,100,0],
-			position: [0,0,0],
-			duration: 250
-		});
-		$timeout(function(){
-			done();
-		},500)
-    },
-    leave: function(element, done){
-		var $elem = $(element);
-	    $elem.css('z-index',100);
-		$elem.snabbt({
-			easing:'easeIn',
-			opacity: 0,
-			fromOpacity: 1,
-			fromPosition: [0,0,0],
-			position: [0,100,0],
-			duration: 250
-		});
-		$timeout(function(){
-			done();
-		},500)
-    }
-  }
+app.animation('.repeated', function($timeout) {
+	return {
+		enter: function(element, done) {
+			var $elem = $(element);
+			$elem.css('position', 'relative');
+			$elem.snabbt({
+				easing: 'spring',
+				springConstant: 0.8,
+				springDeceleration: 0.8,
+				springMass: 10,
+				opacity: 1,
+				fromOpacity: 0,
+				fromPosition: [0, 100, 0],
+				position: [0, 0, 0],
+				duration: 250,
+				fromScale: [0.5, 0.5],
+				scale: [1, 1]
+			});
+			$timeout(function() {
+				done();
+			}, 500)
+		},
+		leave: function(element, done) {
+			var $elem = $(element);
+			$elem.css('display', 'none');
+			$timeout(function() {
+				done();
+			}, 500)
+		}
+	}
 })
 
-function mainCtrl( $scope, $timeout ) {
-
-	var colours = [
-		{
+function mainCtrl($scope, $timeout) {
+	var colours = [{
 			id: 1,
-			name: 'violet',
-			rgb: {
-				r: 148,
-				g: 0,
-				b: 211
-			}
-		}, {
-			id: 3,
-			name: 'blue',
-			rgb: {
-				r: 0,
-				g: 0,
-				b: 255
-			}
-		}, {
-			id: 4,
-			name: 'green',
-			rgb: {
-				r: 0,
-				g: 255,
-				b: 0
-			}
-		}, {
-			id: 5,
-			name: 'yellow',
-			rgb: {
-				r: 255,
-				g: 255,
-				b: 0
-			}
-		}, {
-			id: 6,
-			name: 'orange',
-			rgb: {
-				r: 255,
-				g: 127,
-				b: 0
-			}
-		}, {
-			id: 7,
-			name: 'red',
-			rgb: {
-				r: 255,
-				g: 0,
-				b: 0
-			}
-		} ];
-
-var colours2 = [
-		{
-			id: 7,
-			name: 'red'
+			name: 'violet'
 		}, {
 			id: 3,
 			name: 'blue'
-		}];
-
-
-	var colourVars = [
-			{
-				name: 'violet',
-				rgb: [	{r: 148,g: 0,b: 211}, {r: 148,g: 29,b: 181}, {r: 130,g: 29,b: 229}]
-			},
-			{
-				name: 'blue',
-				rgb: [{r: 0,g: 0,b: 255}, {r: 0,g: 20,b: 255}, {r: 20,g: 10,b: 255}]
-			},
-			{
-				name: 'green',
-				rgb: [{r: 0,g: 255,b: 0}, {r: 29,g: 255,b: 29}, {r: 57,g: 255,b: 37}]
-			},
-			{
-				name: 'yellow',
-				rgb: [{r: 0,g: 255,b: 255}, {r: 27,g: 255,b: 227}, {r: 10,g: 255,b: 188}]
-			},
-			{
-				name: 'orange',
-				rgb: [{r: 255,g: 127,b: 0}, {r: 255,g: 100,b: 27}, {r: 255,g: 117,b: 77}]
-			},
-			{
-				name: 'red',
-				rgb: [{r: 255,g: 0,b: 0}, {r: 255,g: 100,b: 0}, {r: 255,g: 100,b: 100}]
-			}
-		];
-
-var colours3 = _.zipWith( colours, colourVars, function(colorData, colorVar){
-	log(colorVar)
-	var colorShade = colorVar.rgb[_.random(0,colorVar.rgb.length)];
-	colorData.rgb = colorShade;
-	return colorData;
-});
-
-log(colours3)
-
-	var coloursObj = _.map( colours, function( elem ) {
-		return cm.getColorObject( elem.rgb );
-	} );
-
-	$scope.colours = _.zipWith( colours, coloursObj, function( color, colorObj ) {
-		return _.defaults( {
-			colorObj: colorObj,
-			isSelected: false
-		}, color );
-	} );
-
+		}, {
+			id: 4,
+			name: 'green'
+		}, {
+			id: 5,
+			name: 'yellow'
+		}, {
+			id: 6,
+			name: 'orange'
+		}
+		/*, {
+					id: 7,
+					name: 'red'
+				}*/
+	];
+	var colourVars = [{
+			rgb: [{
+				r: 247,
+				g: 0,
+				b: 48
+			}, {
+				r: 199,
+				g: 33,
+				b: 106
+			}, {
+				r: 171,
+				g: 28,
+				b: 139
+			}]
+		}, {
+			rgb: [{
+				r: 1,
+				g: 167,
+				b: 182
+			}, {
+				r: 5,
+				g: 109,
+				b: 189
+			}, {
+				r: 6,
+				g: 84,
+				b: 191
+			}]
+		}, {
+			rgb: [{
+				r: 124,
+				g: 205,
+				b: 13
+			}, {
+				r: 27,
+				g: 183,
+				b: 11
+			}, {
+				r: 144,
+				g: 209,
+				b: 13
+			}]
+		}, {
+			rgb: [{
+				r: 214,
+				g: 202,
+				b: 26
+			}, {
+				r: 255,
+				g: 221,
+				b: 0
+			}, {
+				r: 227,
+				g: 200,
+				b: 28
+			}]
+		}, {
+			rgb: [{
+				r: 255,
+				g: 73,
+				b: 0
+			}, {
+				r: 255,
+				g: 95,
+				b: 0
+			}, {
+				r: 255,
+				g: 116,
+				b: 0
+			}]
+		}
+		/*,
+				{
+					name: 'red',
+					rgb: [{r: 255,g: 23,b: 23}, {r: 224,g: 47,b: 14}, {r: 215,g: 13,b: 37}]
+				}*/
+	];
+	$scope.initColours = function() {
+		var colourShades = _.zipWith(colours, colourVars, function(colorData, colorVar) {
+			var colorShade = colorVar.rgb[_.random(0, colorVar.rgb.length - 1)];
+			colorData.rgb = colorShade;
+			return colorData;
+		});
+		var coloursObj = _.map(colourShades, function(elem) {
+			return cm.getColorObject(elem.rgb);
+		});
+		$scope.colours = _.shuffle(_.zipWith(colourShades, coloursObj, function(color, colorObj) {
+			return _.defaults({
+				colorObj: colorObj,
+				isSelected: false
+			}, color);
+		}));
+	}
 	$scope.currentLevel = 0;
-
-	$scope.levels = [
-		{
-			id: "One",
-			colourCount: 2
-		},{
-			id: "One",
-			colourCount: 2
-		},{
-			id: "One",
-			colourCount: 2
-		}, {
-			id: "Two",
-			colourCount: 3
-		}, {
-			id: "Two",
-			colourCount: 3
-		}, {
-			id: "Two",
-			colourCount: 3
-		}, {
-			id: "Three",
-			colourCount: 4
-		}, {
-			id: "Three",
-			colourCount: 4
-		}, {
-			id: "Three",
-			colourCount: 4
-		}];
-
-	$scope.getCurrentLevelName = function(){
+	$scope.levels = [{
+		id: "One",
+		colourCount: 2
+	}, {
+		id: "One",
+		colourCount: 2
+	}, {
+		id: "One",
+		colourCount: 2
+	}, {
+		id: "Two",
+		colourCount: 3
+	}, {
+		id: "Two",
+		colourCount: 3
+	}, {
+		id: "Two",
+		colourCount: 3
+	}, {
+		id: "Three",
+		colourCount: 4
+	}, {
+		id: "Three",
+		colourCount: 4
+	}, {
+		id: "Three",
+		colourCount: 4
+	}];
+	$scope.getCurrentLevelName = function() {
 		return $scope.levels[$scope.currentLevel].id
 	}
-
-	$scope.getCurrentCount = function(){
-		return $scope.levels[$scope.currentLevel].colourCount
-	}
-
-	$scope.nextRound = function(){
-		if($scope.currentLevel < $scope.levels.length - 1){
+	$scope.getCurrentCount = function() {
+			return $scope.levels[$scope.currentLevel].colourCount
+		}
+		//resets to first round if all current levels are done
+	$scope.nextRound = function() {
+		if ($scope.currentLevel < $scope.levels.length - 1) {
 			$scope.currentLevel++;
 			$scope.initBoard($scope.levels[$scope.currentLevel]);
 		} else {
@@ -240,85 +229,70 @@ log(colours3)
 			$scope.initBoard($scope.levels[$scope.currentLevel]);
 		}
 	}
-
-	$scope.isCorrect = function(){
-		return _.isEqual( sortArrayById( $scope.selectedColours ), sortArrayById( $scope.generatedColours ) );
+	$scope.isCorrect = function() {
+		return _.isEqual(sortArrayById($scope.selectedColours), sortArrayById($scope.generatedColours));
 	}
-
-	$scope.isRoundComplete = function(){
+	$scope.isRoundComplete = function() {
 		var isComplete = ($scope.selectedColours.length == $scope.numberOfColours) && $scope.selectedColours.length > 0;
-		if(isComplete) $scope.$broadcast('round-complete', isComplete);
+		if (isComplete) $scope.$broadcast('round-complete', isComplete);
 		return isComplete;
 	}
-
 	$scope.initBoard = function(levelObj) {
 		var levelObject = levelObj ? levelObj : $scope.levels[0];
-		$scope.colours = _.map( $scope.colours, function( clrObj ) {
-			clrObj.isSelected = false;
-			return clrObj;
-		} );
+		$scope.initColours();
 		$scope.selectedColours = [];
 		$scope.unselectedColours = [];
 		$scope.numberOfColours = levelObject.colourCount;
-		setGoal( $scope.numberOfColours );
-		$timeout( function() {
-			$scope.$broadcast( "update-mix", cm.getColorObject( {
+		setGoal($scope.numberOfColours);
+		$timeout(function() {
+			$scope.$broadcast("update-mix", cm.getColorObject({
 				r: 221,
 				g: 221,
 				b: 221
-			} ).rgbString() );
-		}, 0 );
+			}).rgbString());
+		}, 0);
 	}
-
-	$scope.addColour = function( clrObj ) {
+	$scope.addColour = function(clrObj) {
 		//if colour has been selected, do nothing.
-		if ( _.includes( $scope.selectedColours, clrObj ) ) return;
+		if (_.includes($scope.selectedColours, clrObj)) return;
 		clrObj.isSelected = true;
-		$scope.selectedColours.push( clrObj );
-		$scope.unselectedColours = cm.getUnusedItems( $scope.colours, $scope.selectedColours );
+		$scope.selectedColours.push(clrObj);
+		$scope.unselectedColours = cm.getUnusedItems($scope.colours, $scope.selectedColours);
 		//if this is the first colour is selected, publish as is.
-		if ( $scope.selectedColours.length == 1 ) {
-			$scope.$broadcast( "update-mix", clrObj.colorObj.rgbString() );
+		if ($scope.selectedColours.length == 1) {
+			$scope.$broadcast("update-mix", clrObj.colorObj.rgbString());
 		} else {
 			//else mix all selected colours
-			$scope.$broadcast( "update-mix", cm.mixColours( $scope.selectedColours ).colorObj.rgbString() );
+			$scope.$broadcast("update-mix", cm.mixColours($scope.selectedColours).colorObj.rgbString());
 		}
 	}
 
-	function setGoal( count ) {
-		$scope.generatedColours = cm.getFewRandom( $scope.colours, count );
-		$scope.goal = cm.getColorObject( cm.mixColours( $scope.generatedColours ).colorObj.rgb() ).rgbString();
-		$timeout( function() {
-			$scope.$broadcast( "update-goal", $scope.goal );
-		}, 0 );
-	}
-
-	var sortArrayById = ( function() {
-		return function( array ) {
-			return _.sortBy( array, function( clrObj ) {
-				return clrObj.id;
-			} )
-		}
-	} )();
-
-	$scope.$on( 'round-complete', function( args ) {
+	function setGoal(count) {
+		$scope.generatedColours = cm.getFewRandom($scope.colours, count);
+		$scope.goal = cm.getColorObject(cm.mixColours($scope.generatedColours).colorObj.rgb()).rgbString();
 		$timeout(function() {
-		location.hash = '#modal';
+			$scope.$broadcast("update-goal", $scope.goal);
+		}, 0);
+	}
+	var sortArrayById = (function() {
+		return function(array) {
+			return _.sortBy(array, function(clrObj) {
+				return clrObj.id;
+			})
+		}
+	})();
+	$scope.$on('round-complete', function(args) {
+		$timeout(function() {
+			location.hash = '#modal';
 		}, 500);
 	});
-
-	$scope.$on( 'update-mix', function( args ) {
+	$scope.$on('update-mix', function(args) {
 		$scope.isRoundComplete();
 		$scope.isCorrect();
 	});
-
 	$scope.initBoard($scope.levels[$scope.currentLevel]);
-
 }
 
-function toRgbString( rgbObj ) {
-	log('rgbObj')
-
-	log(rgbObj)
+function toRgbString(rgbObj) {
 	return 'rgb(' + rgbObj.r + ',' + rgbObj.g + ',' + rgbObj.b + ')'
 }
